d8f75198063e3256477f53cc8d7bb4d7f6f4afe2
A. Q. Vassopoli <imagingrelieving633@account.vassopoli.com>
2024-02-06 15:12:08 -0300

    chore: add json logging

--- src/main/java/com/alivassopoli/lambda/TestClass.java
+++ src/main/java/com/alivassopoli/lambda/TestClass.java
@@ -1,12 +1,15 @@
 package com.alivassopoli.lambda;
 
+import org.jboss.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class TestClass implements TestInterface {
+    private static final Logger LOG = Logger.getLogger(TestClass.class);
 
     @Override
     public void execute() {
-        System.out.println("Executing");
+        LOG.info("Executing");
     }
 }
